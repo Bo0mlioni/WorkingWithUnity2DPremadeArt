4e08226a76461627c6580738eaef6fa5f29177fb
jhoran4 <jhoran4@asu.edu>
2022-05-30 18:35:05 -0700

    Player Movement Animation + Movement Smoothing

--- Working 2D With Pre-made Art/Assets/Scenes/Scripts/PlayerMovement.cs
+++ Working 2D With Pre-made Art/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
 
     public PhysicsMaterial2D frictionMoving;
     public PhysicsMaterial2D plentyOfFriction;
@@ -18,14 +19,21 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
 
-        if ((movement.x == 1 || movement.x == -1) || (movement.y == 1 || movement.y == -1))
+        if ((movement.x > 0 || movement.x < -0) || (movement.y > 0 || movement.y < 0))
         {
             rb.sharedMaterial = frictionMoving;
         }
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
+    Vector2 lastReverseForce;
+    int flag = 0;
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Debug.Log(movement.x + " " + movement.y);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); // Move rigidbody according to horizontal/vertical axes and moveSpeed
     }
 }
